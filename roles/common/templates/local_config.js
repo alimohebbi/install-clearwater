43a69b6104a8af72b5e384a9cbbf5a62ad45b7fe
Ali Mohebbi <amohebbi@star-cloud.inf.usi.ch>
2019-07-03 18:17:03 +0200

    Initial commit

---
+++ roles/common/templates/local_config.js
@@ -0,0 +1,5 @@
+local_ip={{hostvars[ansible_host]['local_ip']}}
+public_ip={{hostvars[ansible_host]['public_ip']}}
+public_hostname={{hostvars[ansible_host]['host_name']}}.curix.ch
+etcd_cluster={{etcd_address}}
+
